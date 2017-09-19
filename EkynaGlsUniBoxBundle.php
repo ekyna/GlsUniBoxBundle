c5556e53cf912ba452a59c982f9099cbe69a1be7
Etienne Dauvergne <contact@ekyna.com>
2017-09-19 23:19:53 +0200

    [GlsUniBox] Initial commit

---
+++ EkynaGlsUniBoxBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ekyna\Bundle\GlsUniBoxBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class EkynaGlsUniBoxBundle
+ * @package Ekyna\Bundle\GlsUniBoxBundle
+ * @author  Etienne Dauvergne <contact@ekyna.com>
+ */
+class EkynaGlsUniBoxBundle extends Bundle
+{
+}
